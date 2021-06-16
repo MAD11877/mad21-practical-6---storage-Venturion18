0cbb9bea7430fa847dd4ffb715859097ac73899a
Venturion18 <s10204931@connect.np.edu.sg>
2021-06-17 02:05:25 +0800

    Submission

---
+++ app/src/main/java/main_activity/ListActivity.java
@@ -0,0 +1,15 @@
+package main_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import main_activity.R;
+
+public class ListActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
